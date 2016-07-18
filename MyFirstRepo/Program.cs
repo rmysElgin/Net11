ed36f137286d91d70dfca152ecd5d7ac4ead03da
rmysElgin <rmys2650@gmail.com>
2016-07-18 15:08:30 +0300

    first commit 2016.07.18 15:08

---
+++ MyFirstRepo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //github hy6nu6mu6mu
+        }
+    }
+}
